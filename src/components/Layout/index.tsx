7a96b8cca42edfe943e0dc6e9b72a5c4a38b81f1
David Christian <david.w.christian@gmail.com>
2021-02-11 17:57:53 -0800

    Added initial metamask ui

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,6 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { Link as ReactLink } from 'react-router-dom';
 import Link from '@material-ui/core/Link';
+import Container from '@material-ui/core/Container';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -16,8 +17,37 @@ const useStyles = makeStyles((theme) => ({
 	appBar: {
 		backgroundColor: 'white',
 		boxShadow: 'none',
-		borderBottom: `1px solid #ddd`,
+		borderBottom: `1px solid #eceff1`,
 		color: '#bbb',
+	},
+	metaMaskContainer: {
+		display: 'flex',
+		flexDirection: 'row',
+		alignItems: 'center',
+		backgroundColor: '#eceff1',
+		borderRadius: '12px',
+	},
+	addressContainer: {
+		border: `1px solid #eceff1`,
+		borderRadius: '12px',
+		backgroundColor: 'white',
+		zIndex: 3,
+	},
+	accountInfo: {
+		padding: '.3rem .5rem',
+		color: theme.palette.text.primary,
+	},
+	button: {
+		// border: '1px solid rgba(111,76,255,1.0)',
+		// boxShadow: '0 0 24px 0 rgb(111 76 255 / 32%)',
+		padding: '15px 25px',
+		color: 'rgba(255,255,255,0.96)',
+		borderRadius: '4px',
+		background: 'rgba(111,76,255,0.1)',
+		transition: 'all 0.3s ease',
+		opacity: '1',
+		border: '2px solid rgb(166,150,231)',
+		boxShadow: ' 0 0 24px 0 rgb(111 76 255 / 32%)',
 	},
 }));
 
@@ -36,18 +66,30 @@ const Layout: React.FC<Props> = ({ children }) => {
 					root: classes.appBar,
 				}}
 			>
-				<Toolbar>
-					<Typography variant="h6" className={classes.title}>
-						<Link
-							to="/"
-							color="primary"
-							underline="none"
-							component={ReactLink}
-						>
-							DApp
-						</Link>
-					</Typography>
-				</Toolbar>
+				<Container>
+					<Toolbar>
+						<Typography variant="h6" className={classes.title}>
+							<Link
+								to="/"
+								color="primary"
+								underline="none"
+								component={ReactLink}
+							>
+								DApp
+							</Link>
+						</Typography>
+						<div className={classes.metaMaskContainer}>
+							<Typography className={classes.accountInfo}>
+								0.3067 ETH
+							</Typography>
+							<div className={classes.addressContainer}>
+								<Typography className={classes.accountInfo}>
+									0x9881...CC0D
+								</Typography>
+							</div>
+						</div>
+					</Toolbar>
+				</Container>
 			</AppBar>
 			{children}
 		</div>
--- src/hooks/useIsMetaMaskInstalled.ts
+++ src/hooks/useIsMetaMaskInstalled.ts
@@ -4,10 +4,23 @@ import detectEthereumProvider from '@metamask/detect-provider';
 const useIsMetaMaskInstalled = () => {
 	useEffect(() => {
 		const detectProvider = async () => {
-			const provider = await detectEthereumProvider();
+			const provider: any = await detectEthereumProvider();
 
 			if (provider) {
-				console.log('MetaMask Installed!!');
+				provider
+					.request({ method: 'eth_requestAccounts' })
+					.then((accounts: any) => {
+						console.log(accounts);
+					})
+					.catch((err: any) => {
+						if (err.code === 4001) {
+							// EIP-1193 userRejectedRequest error
+							// If this happens, the user rejected the connection request.
+							console.log('Please connect to MetaMask.');
+						} else {
+							console.error(err);
+						}
+					});
 			} else {
 				console.log('Please install MetaMask!');
 			}
