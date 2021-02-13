5022541fc5616ca97962b2a4f45b900dcbb1dd53
David Christian <david.w.christian@gmail.com>
2021-02-13 12:52:02 -0800

    Initial connect wallet button styling

--- src/components/buttons/ConnectToWallet/index.tsx
+++ src/components/buttons/ConnectToWallet/index.tsx
@@ -1,17 +1,15 @@
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
-	metaMaskConnectContainer: {
-		display: 'flex',
-		flexDirection: 'row',
-		alignItems: 'center',
+	buttonRoot: {
+		textTransform: 'none',
 		borderRadius: '12px',
 		border: `1px solid #eceff1`,
 	},
-	accountInfo: {
-		padding: '.3rem .5rem',
-		color: theme.palette.text.primary,
+	buttonText: {
+		padding: '.2rem .4rem',
 	},
 }));
 
@@ -19,11 +17,13 @@ const ConnectToWallet = () => {
 	const classes = useStyles();
 
 	return (
-		<div className={classes.metaMaskConnectContainer}>
-			<Typography className={classes.accountInfo}>
-				Connect to a wallet
-			</Typography>
-		</div>
+		<>
+			<Button className={classes.buttonRoot} disableRipple={true}>
+				<Typography className={classes.buttonText}>
+					Connect to a wallet
+				</Typography>
+			</Button>
+		</>
 	);
 };
 
