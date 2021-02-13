5fef38c506b55fcb72347dda80291372985e90fa
David Christian <david.w.christian@gmail.com>
2021-02-13 11:29:07 -0800

    Fixed bug logo component

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,13 +1,24 @@
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import { Link as ReactLink } from 'react-router-dom';
 import Link from '@material-ui/core/Link';
 
+const useStyles = makeStyles(() => ({
+	title: {
+		flexGrow: 1,
+	},
+}));
+
 const Logo = () => {
-	<Typography variant="h6" className={classes.title}>
-		<Link to="/" color="primary" underline="none" component={ReactLink}>
-			DApp
-		</Link>
-	</Typography>;
+	const classes = useStyles();
+
+	return (
+		<Typography variant="h6" className={classes.title}>
+			<Link to="/" color="primary" underline="none" component={ReactLink}>
+				DApp
+			</Link>
+		</Typography>
+	);
 };
 
 export default Logo;
