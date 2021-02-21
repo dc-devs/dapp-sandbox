49b98c958a18a2e5606f57fd0e09d9abfd8ab52f
David Christian <david.w.christian@gmail.com>
2021-02-21 10:35:14 -0800

    Added ETHERSCAN_API_KEY env var

--- src/services/etherscan/get-transactions.ts
+++ src/services/etherscan/get-transactions.ts
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
 // TODO: Fix ENV vars
-// const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
-const ETHERSCAN_API_KEY = 'PFYD32UAM9SWUMEMK6ADUU38QYYY9UWYTA';
+const REACT_APP_ETHERSCAN_API_KEY = process.env.REACT_APP_ETHERSCAN_API_KEY;
 
 const getTransactions = async () => {
-	console.log('ETHERSCAN_API_KEY', ETHERSCAN_API_KEY);
-	const url = `http://api.etherscan.io/api?module=account&action=txlist&address=0x98818d16dE85fc14681e865389E77dBdEBa4CC0D&startblock=0&endblock=99999999&sort=asc&apikey=${ETHERSCAN_API_KEY}`;
+	const url = `http://api.etherscan.io/api?module=account&action=txlist&address=0x98818d16dE85fc14681e865389E77dBdEBa4CC0D&startblock=0&endblock=99999999&sort=asc&apikey=${REACT_APP_ETHERSCAN_API_KEY}`;
 
 	// Replace with types
 	const response: any = await axios.get(url);
