47d6d1f71172ca338a8bacc7088fb54a2bf723ef
David Christian <david.w.christian@gmail.com>
2021-05-25 01:40:23 -0700

    Updated pie chart UI

--- src/app/screens/dashboard/components/asset-pie-chart/asset-pie-chart.tsx
+++ src/app/screens/dashboard/components/asset-pie-chart/asset-pie-chart.tsx
@@ -1,15 +1,9 @@
+import { useState } from 'react';
 import numeral from 'numeral';
 import pieChartColors from './pie-chart-colors';
 import { makeStyles } from '@material-ui/core/styles';
 import generateSeriesData from './generate-series-data';
-import {
-	Pie,
-	Cell,
-	Label,
-	Legend,
-	PieChart,
-	ResponsiveContainer,
-} from 'recharts';
+import { Pie, Cell, Label, Sector, Legend, PieChart } from 'recharts';
 import TokenDisplayData from '../../../../../interfaces/token-display-data-interface';
 
 // LEFT OFF
@@ -22,10 +16,16 @@ const useStyles = makeStyles((theme) => ({
 		color: theme.palette.primary.main,
 		fill: theme.palette.primary.main,
 	},
-	legendContainer: {},
+	legendContainer: {
+		display: 'flex',
+		flexWrap: 'wrap',
+		justifyContent: 'center',
+	},
 	legendItemContainer: {
 		display: 'flex',
 		alignItems: 'center',
+		justifyContent: 'center',
+		margin: '2px 30px',
 	},
 	legendCircle: {
 		width: '10px',
@@ -38,9 +38,17 @@ const useStyles = makeStyles((theme) => ({
 		alignItems: 'center',
 		justifyContent: 'space-between',
 		width: '100px',
+		fontSize: '.95rem',
 	},
 	legendItemToken: {
 		marginRight: '10px',
+	},
+	pieChart: {
+		cursor: 'pointer',
+	},
+	labelContainer: {
+		display: 'flex',
+		flexDirection: 'column',
 	},
 }));
 
@@ -65,6 +73,17 @@ interface TokenSymbolPercents {
 const AssetPieChart = ({ totalAssetValue, tokenDisplayData }: Props) => {
 	const classes = useStyles();
 	const seriesData = generateSeriesData({ tokenDisplayData });
+	const cellComponents = seriesData.map((entry, index) => {
+		return <Cell key={`cell-${index}`} fill={pieChartColors[index]} />;
+	});
+	const [index, setIndex] = useState(-1);
+
+	const onPieEnter = (_: any, index: number) => {
+		setIndex(index);
+	};
+	const onPieLeave = () => {
+		setIndex(-1);
+	};
 
 	const legendComponent = (props: any) => {
 		const { payload } = props;
@@ -74,8 +93,6 @@ const AssetPieChart = ({ totalAssetValue, tokenDisplayData }: Props) => {
 			const tokenData = token.payload;
 			tokenSymbolsPercents[tokenData.name] = tokenData.percent;
 		});
-
-		console.log(tokenSymbolsPercents);
 
 		const legendItems = payload.map((entry: any, index: number) => {
 			let colorHex;
@@ -122,23 +139,102 @@ const AssetPieChart = ({ totalAssetValue, tokenDisplayData }: Props) => {
 		return <div className={classes.legendContainer}>{legendItems}</div>;
 	};
 
-	const cellComponents = seriesData.map((entry, index) => {
-		return <Cell key={`cell-${index}`} fill={pieChartColors[index]} />;
-	});
+	const renderActiveShape = (props: any) => {
+		const RADIAN = Math.PI / 180;
+		const {
+			cx,
+			cy,
+			midAngle,
+			innerRadius,
+			outerRadius,
+			startAngle,
+			endAngle,
+			fill,
+			payload,
+			percent,
+			value,
+		} = props;
+		const sin = Math.sin(-RADIAN * midAngle);
+		const cos = Math.cos(-RADIAN * midAngle);
+		const sx = cx + (outerRadius + 10) * cos;
+		const sy = cy + (outerRadius + 10) * sin;
+		const mx = cx + (outerRadius + 30) * cos;
+		const my = cy + (outerRadius + 30) * sin;
+		const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+		const ey = my;
+		const textAnchor = cos >= 0 ? 'start' : 'end';
+
+		const tokenAssetValue = numeral(payload.value).format('$0,000');
+		const tokenAssetSymbol = payload.name;
+		const tokenPercent = percent * 100;
+		const percentFormatted = `${numeral(tokenPercent).format('0.0')}%`;
+
+		return (
+			<g>
+				<text
+					x={cx}
+					y={cy - 17}
+					dy={8}
+					textAnchor="middle"
+					fill={fill}
+					className={classes.label}
+				>
+					{tokenAssetSymbol}
+				</text>
+				<text
+					x={cx}
+					y={cy + 17}
+					dy={8}
+					textAnchor="middle"
+					fill={fill}
+					className={classes.label}
+				>
+					{tokenAssetValue}
+				</text>
+				<Sector
+					cx={cx}
+					cy={cy}
+					cornerRadius={100}
+					innerRadius={innerRadius}
+					outerRadius={outerRadius}
+					startAngle={startAngle}
+					endAngle={endAngle}
+					fill={fill}
+				/>
+				<Sector
+					cx={cx}
+					cy={cy}
+					cornerRadius={100}
+					startAngle={startAngle}
+					endAngle={endAngle}
+					innerRadius={outerRadius + 6}
+					outerRadius={outerRadius + 10}
+					fill={fill}
+				/>
+			</g>
+		);
+	};
 
 	return (
-		<ResponsiveContainer width={400} height={300}>
-			<PieChart>
-				<Pie
-					cx="32%"
-					data={seriesData}
-					innerRadius={115}
-					outerRadius={130}
-					paddingAngle={0}
-					cornerRadius={50}
-					dataKey="value"
-				>
-					{cellComponents}
+		<PieChart width={465} height={390} className={classes.pieChart}>
+			<Pie
+				cy={140}
+				cursor="pointer"
+				activeIndex={index}
+				onMouseEnter={onPieEnter}
+				onMouseLeave={onPieLeave}
+				activeShape={renderActiveShape}
+				data={seriesData}
+				innerRadius={120}
+				outerRadius={130}
+				paddingAngle={0}
+				cornerRadius={100}
+				dataKey="value"
+				className={classes.pieChart}
+			>
+				{cellComponents}
+
+				{index === -1 ? (
 					<Label
 						width={30}
 						position="center"
@@ -146,15 +242,16 @@ const AssetPieChart = ({ totalAssetValue, tokenDisplayData }: Props) => {
 					>
 						{totalAssetValue}
 					</Label>
-				</Pie>
-				<Legend
-					content={legendComponent}
-					align="right"
-					layout="vertical"
-					verticalAlign="top"
-				/>
-			</PieChart>
-		</ResponsiveContainer>
+				) : (
+					''
+				)}
+			</Pie>
+			<Legend
+				align="center"
+				layout="horizontal"
+				content={legendComponent}
+			/>
+		</PieChart>
 	);
 };
 
