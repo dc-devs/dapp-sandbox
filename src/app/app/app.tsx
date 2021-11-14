3c0d2b282ff79d72741038331674fabb85eb8641
David Christian <david.w.christian@gmail.com>
2021-11-14 11:14:44 -0800

    Fixed bug in wallet connect

--- src/app/app/app.tsx
+++ src/app/app/app.tsx
@@ -3,25 +3,43 @@ import AppHome from '../screens/app-home';
 import { useSelector, useDispatch } from 'react-redux';
 import LayoutAppHome from '../layouts/layout-app-home';
 import { Route, Switch } from 'react-router-dom';
-import { fetchMetaMaskWallet } from '../../redux/slices/metamask-slice';
+import {
+	fetchMetaMaskWallet,
+} from '../../redux/slices/metamask-slice';
 import {
 	selectIsMetaMaskConnected,
 	fetchIsMetaMaskConnected,
 } from '../../redux/slices/metamask-connected-slice';
-import { fetchIsMetaMaskInstalled } from '../../redux/slices/metamask-installed-slice';
+import {
+	selectIsMetaMaskInstalled,
+	fetchIsMetaMaskInstalled,
+} from '../../redux/slices/metamask-installed-slice';
 
 const App = () => {
 	const dispatch = useDispatch();
+	// const metaMaskWallet = useSelector(selectMetaMaskWallet);
+	const isMetaMaskInstalled = useSelector(selectIsMetaMaskInstalled);
 	const isMetaMaskConnected = useSelector(selectIsMetaMaskConnected);
 
-	dispatch(fetchIsMetaMaskInstalled());
 	dispatch(fetchIsMetaMaskConnected());
+
+	// console.log('App - metaMaskWallet', metaMaskWallet);
+	// console.log('App - isMetaMaskInstalled', isMetaMaskInstalled);
+	// console.log('App - isMetaMaskConnected', isMetaMaskConnected);
 
 	useEffect(() => {
 		if (isMetaMaskConnected) {
 			dispatch(fetchMetaMaskWallet());
 		}
-	}, [isMetaMaskConnected, dispatch]);
+
+		if (!isMetaMaskInstalled) {
+			dispatch(fetchIsMetaMaskInstalled());
+		}
+
+		if (isMetaMaskInstalled && !isMetaMaskConnected) {
+			dispatch(fetchIsMetaMaskConnected());
+		}
+	}, [isMetaMaskInstalled, isMetaMaskConnected, dispatch]);
 
 	return (
 		<>
