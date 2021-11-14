7f0990472eda727b98524a8cffb02d2bcb030808
David Christian <david.w.christian@gmail.com>
2021-11-13 21:28:29 -0800

    Removed old code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,3 @@
-import splitArrayInTwo from './split-array-in-two';
-import filterTokenBalances from './filter-token-balances';
 import shortenWalletAddress from './shorten-wallet-address/shorten-wallet-address';
 
-export { splitArrayInTwo, filterTokenBalances, shortenWalletAddress };
+export { shortenWalletAddress };
